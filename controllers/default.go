2b60500e534a985adee28db8e1a5ce6d01417e1d
Sergio Palechor <davidPalechor95@hotmail.com>
2018-09-07 17:22:30 -0500

    Update

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/httplib"
 )
 
 type MainController struct {
@@ -12,4 +14,21 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+}
+
+type ApiController struct {
+	beego.Controller
+}
+
+type jsonResponse struct {
+	Name string `json:"name"`
+}
+
+func (c *ApiController) Get() {
+	req := httplib.Get("http://api.openweathermap.org/data/2.5/weather?q=Bogota,co&appid=1508a9a4840a5574c822d70ca2132032")
+	data, _ := req.String()
+	fmt.Printf("%q\n", data)
+	jsonResponse := jsonResponse{"Sergio David"}
+	c.Data["json"] = &jsonResponse
+	c.ServeJSON()
 }
